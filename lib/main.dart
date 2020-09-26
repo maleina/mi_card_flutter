324a018a46681c65376551b1dafce6b5bcc4f1c8
Maleina Bidek <maleina@ucla.edu>
2020-09-26 13:17:50 -0400

    feat: Add photo, name, title

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,46 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+      MyApp()
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/maleina.jpeg'),
+              ),
+              Text(
+                'Maleina Bidek',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'APP DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 20.0,
+                  color: Colors.teal[100],
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
